e28cf85b848d365a7c54c3adf9f0f113d281b3f1
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-09-14 12:59:12 +0530

    Time: 0 ms (100%), Space: 42.6 MB (52.92%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        // Remove Math.abs() so negative positions evaluate properly
         int mH = Math.max(0, Math.min(rec1[2], rec2[2]) - Math.max(rec1[0], rec2[0]));
         int mW = Math.max(0, Math.min(rec1[3], rec2[3]) - Math.max(rec1[1], rec2[1]));
     
